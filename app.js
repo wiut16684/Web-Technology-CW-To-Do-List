0c9f251e998b04967c4af2ee473e6c2580d76278
wiut16684 <wiut16684@gmail.com>
2024-03-12 18:41:15 +0500

    started the project by creating files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, () => {
+    console.log(`The application is running on port ${port}`);
+});
