dd00861cd566ebdb3078eb54345fed2d76bba7b6
wiut16684 <wiut16684@gmail.com>
2024-03-12 23:02:18 +0500

    User can create tasks now thanks to POST, and i have also added some instruction on readme file

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Web-Technology-CW-To-Do-List
+
 This is the application created to for CW
+
+# Instructions
+
+1. Download the source code from github
+
+```bash
+
+git clone https://github.com/wiut16684/Web-Technology-CW-To-Do-List.git
+
+```
+
+or just download the zip file using the url of repository
+
+2. Install dependencies
+
+```bash
+npm i
+```
+
+or
+
+```bash
+npm install
+```
+
+3. Running the program
+
+```bash
+node app.js
+```
+
+or
+
+```bash
+nodemon app.js
+```
+
+or
+
+```bash
+npm start
+```
+
+4. Dependencies list
+
+-express
+-pug
+
+### Link for Github repository
+
+https://github.com/wiut16684/Web-Technology-CW-To-Do-List.git
--- app.js
+++ app.js
@@ -1,14 +1,62 @@
+const { json } = require("body-parser");
+const { error } = require("console");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const fs = require("fs");
+
 app.set("view engine", "pug");
 app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.render("home");
+    fs.readFile("./data/todos.json", (err, data) => {
+        if (err) {
+            console.error("Error reading todos:", err);
+            res.render("home", { todos: [] });
+            return;
+        }
+        const todos = JSON.parse(data);
+        res.render("home", { todos: todos });
+    });
+});
+
+app.post("/add", (req, res) => {
+    const formData = req.body;
+    if (formData.todo.trim() == "") {
+        fs.readFile("./data/todos.json", (err, data) => {
+            if (err) throw err;
+            const todos = JSON.parse(data);
+            res.render("home", { error: true, todos: todos });
+        });
+    } else {
+        fs.readFile("./data/todos.json", (err, data) => {
+            if (err) throw err;
+
+            const todos = JSON.parse(data);
+
+            const todo = {
+                id: id(),
+                description: formData.todo,
+                done: false,
+            };
+
+            todos.push(todo);
+
+            fs.writeFile("./data/todos.json", JSON.stringify(todos), (err) => {
+                if (err) throw err;
+
+                res.redirect("/");
+            });
+        });
+    }
 });
 
 app.listen(port, () => {
     console.log(`The application is running on port ${port}`);
 });
+
+function id() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+}
