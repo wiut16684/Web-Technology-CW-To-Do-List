7d19e9c49e8b732b99eeabeaf036551201255de3
wiut16684 <wiut16684@gmail.com>
2024-03-13 20:22:23 +0500

    Added error handling and some style also edited some codes for cleaning. Moreover added some more information in readme file

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Web-Technology-CW-To-Do-List
 
 This is the application created to for CW
+I have created To Application using node/express.js where users able to read, add, remove and update the tasks. I have used pug as template engine as it gives more readability and clean code writing chances.
+
+Below i will give some instruction how to set up my Program
 
 # Instructions
 
@@ -26,7 +29,7 @@ or
 npm install
 ```
 
-3. Running the program
+3. Run the program
 
 ```bash
 node app.js
@@ -52,3 +55,14 @@ npm start
 ### Link for Github repository
 
 https://github.com/wiut16684/Web-Technology-CW-To-Do-List.git
+
+### Program Functions
+
+There is an add button that users can use to add new tasks, and also delate where users can delate unwanted tasks
+
+In order to update, just press the task so it will be updated
+
+### Project structure
+
+I have used simple clear structure for my application which contains data, public, views, node_modules folders. In data there is json file which user tasks are stured. In public folder I have added my main css file, to give styles for all the elements in the application.
+In views there is 2 pug files which one contains head pard and the other contains body part of my templete.
--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const { json } = require("body-parser");
-const { error } = require("console");
-const express = require("express");
+import express from "express";
+import fs from "fs";
 const app = express();
 const port = 3000;
 
-const fs = require("fs");
-
+// Seting up middlewares
 app.set("view engine", "pug");
 app.use("/static", express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+// Home route
 app.get("/", (req, res) => {
     fs.readFile("./data/todos.json", (err, data) => {
         if (err) {
@@ -22,17 +21,28 @@ app.get("/", (req, res) => {
     });
 });
 
+// Adding task route
 app.post("/add", (req, res) => {
     const formData = req.body;
     if (formData.todo.trim() == "") {
+        // If the todo is empty, render home with an error
         fs.readFile("./data/todos.json", (err, data) => {
-            if (err) throw err;
+            if (err) {
+                console.error("Error reading todos:", err);
+                res.render("home", { error: true, todos: [] });
+                return;
+            }
             const todos = JSON.parse(data);
             res.render("home", { error: true, todos: todos });
         });
     } else {
+        // If the todo is not empty, add it to the list and redirect to home
         fs.readFile("./data/todos.json", (err, data) => {
-            if (err) throw err;
+            if (err) {
+                console.error("Error reading todos:", err);
+                res.status(500).send("Internal Server Error");
+                return;
+            }
 
             const todos = JSON.parse(data);
 
@@ -45,7 +55,11 @@ app.post("/add", (req, res) => {
             todos.push(todo);
 
             fs.writeFile("./data/todos.json", JSON.stringify(todos), (err) => {
-                if (err) throw err;
+                if (err) {
+                    console.error("Error writing todos:", err);
+                    res.status(500).send("Internal Server Error");
+                    return;
+                }
 
                 res.redirect("/");
             });
@@ -53,27 +67,69 @@ app.post("/add", (req, res) => {
     }
 });
 
+// Deleting task route
 app.get("/:id/delete", (req, res) => {
     const id = req.params.id;
     fs.readFile("./data/todos.json", (err, data) => {
-        if (err) throw err;
+        if (err) {
+            console.error("Error reading todos:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
         const todos = JSON.parse(data);
         const filteredTodos = todos.filter((todo) => todo.id != id);
         fs.writeFile(
             "./data/todos.json",
             JSON.stringify(filteredTodos),
             (err) => {
-                if (err) throw err;
+                if (err) {
+                    console.error("Error writing todos:", err);
+                    res.status(500).send("Internal Server Error");
+                    return;
+                }
                 res.redirect("/");
             }
         );
     });
 });
 
+// Updating task route
+app.get("/:id/update", (req, res) => {
+    const id = req.params.id;
+    fs.readFile("./data/todos.json", (err, data) => {
+        if (err) {
+            console.error("Error reading todos:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+
+        const todos = JSON.parse(data);
+        const todo = todos.find((todo) => todo.id === id);
+
+        if (!todo) {
+            res.status(404).send("Todo not found");
+            return;
+        }
+
+        todo.done = true;
+
+        fs.writeFile("./data/todos.json", JSON.stringify(todos), (err) => {
+            if (err) {
+                console.error("Error writing todos:", err);
+                res.status(500).send("Internal Server Error");
+                return;
+            }
+            res.redirect("/");
+        });
+    });
+});
+
+// Starting the server
 app.listen(port, () => {
     console.log(`The application is running on port ${port}`);
 });
 
+// Random ID generator function
 function id() {
     return "_" + Math.random().toString(36).substr(2, 9);
 }
